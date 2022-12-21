94ac577ad67e5b454cf1603e1af5b79c7badf573
MichaLL27 <Lazarashvili2704@gmail.com>
2022-12-21 08:30:52 +0400

    create new item

--- src/app/newProduct/new.product.ts
+++ src/app/newProduct/new.product.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ItemsService } from '../items.service';
 
 @Component({
   templateUrl: 'new.product.html',
@@ -18,15 +19,43 @@ export class NewProduct implements OnInit {
     date: new FormControl(''),
   });
 
-  constructor(private router: Router) {}
+  Name = this.newProduct.controls.name.value;
+  ProductNumber = this.newProduct.controls.productNumber.value;
+  Color = this.newProduct.controls.color.value;
+  StandartCost = +this.newProduct.controls.color.value;
+  Price = this.newProduct.controls.standartCost.value;
+  Size = +this.newProduct.controls.size.value;
+  Weight = +this.newProduct.controls.weight.value;
+  Date = this.newProduct.controls.date.value;
 
-  @Output() newItem: EventEmitter<string> = new EventEmitter();
+  constructor(private router: Router, private items: ItemsService) {}
 
   ngOnInit() {}
 
-  onNewItem() {}
+  onNewItem() {
+    const createItem = [
+      {
+        id: 21,
+        name: this.newProduct.controls.name.value,
+        number: this.newProduct.controls.productNumber.value,
+        color: this.newProduct.controls.color.value,
+        price: this.newProduct.controls.price.value,
+        date: this.newProduct.controls.date.value,
+        actions: 'add',
+        cost: +this.newProduct.controls.standartCost.value,
+        size: +this.newProduct.controls.size.value,
+        weight: +this.newProduct.controls.weight.value,
+      },
+    ];
+
+    createItem.map((x) => {
+      this.items.items.unshift(x);
+    });
+    this.items.fixIds();
+    this.router.navigate(['/products']);
+  }
 
   calncel() {
-    this.router.navigateByUrl('/products');
+    this.router.navigate(['/products']);
   }
 }
